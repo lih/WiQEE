35ae3c28a91bff09bc96a9f81d2a161a473b9d6a
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2019-04-11 19:30:16 +0200

    Pages updated (on Thu Apr 11 19:30:15 CEST 2019)

--- static/theme-tweak.js
+++ static/theme-tweak.js
@@ -1,8 +1,28 @@
 function rawUnit(u) { return function (x) { return x.toString() + u; }; }
 
-var Theme = {
-    'props': JSON.parse(localStorage.getItem('theme/'+window.location.pathname)),
-    'propUnits': {
+var ThemeConfig = {
+    defaultAmbiance: 'day',
+    defaultProps: {
+	'day': {
+	    'light': true,
+	    '--prim-hue': 53, '--prim-saturation': 36,
+	    '--low-hue': -50, '--low-saturation': 30,
+	    '--high-hue': 50, '--high-saturation': 30,
+	    '--min-contrast': 50, '--max-contrast': 85,
+	    '--luminosity': 50,
+	    'font-size': 100
+	},
+	
+	'night': {
+	    'light': false,
+	    '--prim-hue': 53, '--prim-saturation': 36,
+	    '--low-hue': -50, '--low-saturation': 30,
+	    '--high-hue': 50, '--high-saturation': 30,
+	    '--min-contrast': 50, '--max-contrast': 85,
+	    '--luminosity': 50,
+	}
+    },
+    propUnits: {
 	'--luminosity': function (n) { return (n / 100).toString(); },
 	'--prim-hue': rawUnit('deg'),
 	'--low-hue': rawUnit('deg'),
@@ -14,70 +34,88 @@ var Theme = {
 	'--max-contrast': rawUnit('%'),
 	'font-size': rawUnit('%'),
     },
-    'propNames': ['--prim-hue','--prim-saturation','--low-hue','--low-saturation','--high-hue','--high-saturation', '--min-contrast', '--max-contrast', '--luminosity', 'font-size'],
-    'setProp': function (e,prop,val) {
-	var oldval = this.props[prop];
-	this.props[prop] = val;
-	localStorage.setItem('theme/'+window.location.pathname,JSON.stringify(this.props));
 
-	if(prop == 'light') {
-	    if(oldval != this.props[prop]) {
-		var lst = e.classList; lst.toggle('ground-dark'); lst.toggle('ground-light');
-	    }
-	}
-	else {
-	    var stl = "", p;
-	    for(var p in this.propNames) {
-		var prop = this.propNames[p];
-		stl = stl + " " + prop+': '+this.propUnits[prop](this.props[prop]) + ";"
-	    }
-	    e.setAttribute('style', stl);
+    propNames = ['--prim-hue','--prim-saturation',
+		 '--low-hue','--low-saturation',
+		 '--high-hue','--high-saturation',
+		 '--min-contrast', '--max-contrast',
+		 '--luminosity',
+		 'font-size'],
+};
 
-	    var spans = e.getElementsByClassName('theme-prop-display');
-	    for (var spanI in spans) {
-		var span = spans.item(spanI);
-		if(span !== null) {
-		    var prop = span.getAttribute('data-prop-name');
-		    span.textContent = this.propUnits[prop](this.props[prop]);
-		}
-	    }
-	}
+function setGroundClass(e,isLight) {
+    if(isLight) {
+	root.classList.replace('ground-dark', 'ground-light');
+    }
+    else {
+	root.classList.replace('ground-light', 'ground-dark');
+    }
+}
+
+var Theme = {
+    roots: [],
+    config: ThemeConfig,
+    props: JSON.parse(window.localStorage.getItem('theme/'+window.location.pathname)) || ThemeConfig.defaultProps,
+    ambiance: window.localStorage.getItem('ambiance/'+window.location.pathname) || ThemeConfig.defaultAmbiance,
+    
+    attach: function (root) {
+	this.roots.push(root);
+	var rootI = this.roots.length - 1;
+
+	setGroundClass(root,this.getPropVal('light')); 
 	
-
-    },
-
-    'attach': function (e) {
-	this.setProp(e,'font-size',this.props['font-size']);
-	
-	if(! this.props['light']) {
-	    e.classList.toggle('ground-dark');
-	    e.classList.toggle('ground-light');
-	}
-
-	var elts = e.getElementsByClassName('theme-slider');
+	var elts = root.getElementsByClassName('theme-slider');
 	for(var x in elts) {
 	    var elt = elts.item(x);
 	    (function (tw,elt,slide) {
 		var th_val = elt.getElementsByClassName('theme-prop-display')[0];
 		var prop = th_val.getAttribute('data-prop-name');
-		slide.value = parseInt(tw.props[prop]);
+		slide.value = tw.getPropVal(prop);
 		slide.addEventListener('input',function () {
 		    tw.setProp(e,prop,this.value);
 		});
 	    })(this,elt,elt.getElementsByTagName('input')[0]);
 	}
+
+	return rootI;
+    },
+    getPropVal: function(p) {
+	return this.props[this.ambiance][p];
+    },
+    getPropText: function(p) {
+	return this.config.propUnits[p](this.getPropVal(p));
+    },
+    setProp: function (p,v) {
+	this.props[this.ambiance][p] = v;
+	return this;
+    },
+    getStyleText: function() {
+	var stl = "", p;
+	for(var p in this.config.propNames) {
+	    var prop = this.config.propNames[p];
+	    stl = stl + " " + prop+': '+this.getPropText(prop) + ";"
+	}
+	return stl;
+    },
+    updateElement: function (i) {
+	var elt = this.roots[i];
+	elt.setAttribute('style', this.getStyleText());
+	setGroundClass(elt.getPropVal('light'));
+	
+	var spans = elt.getElementsByClassName('theme-prop-display');
+	for (var spanI in spans) {
+	    var span = spans.item(spanI);
+	    if(span !== null) {
+		var prop = span.getAttribute('data-prop-name');
+		span.textContent = this.getPropText(prop);
+	    }
+	}
+    },
+    updateAll: function() {
+	for(var i=0;i<this.roots.length; i++) {
+	    this.updateElement(i);
+	}
     }
 };
 
-if(Theme.props === null) {
-    Theme.props = {
-	'light': true,
-	'--prim-hue': 53, '--prim-saturation': 36,
-	'--low-hue': -50, '--low-saturation': 30,
-	'--high-hue': 50, '--high-saturation': 30,
-	'--min-contrast': 50, '--max-contrast': 85,
-	'--luminosity': 50,
-	'font-size': 100
-    };
-}
 
