b6eb53d4924243ca395a14f0f0051ca533d6fe2b
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2019-04-11 23:32:42 +0200

    Pages updated (on Thu Apr 11 23:32:42 CEST 2019)

--- static/theme-tweak.js
+++ static/theme-tweak.js
@@ -121,6 +121,7 @@ var Theme = {
 	    return;
 	}
 	var elt = this.roots[i];
+	elt.classList.toggle('smooth-colors');
 	elt.setAttribute('style', this.getStyleText());
 	setLightGround(elt,this.getPropVal('light'));
 	
@@ -132,6 +133,8 @@ var Theme = {
 		span.textContent = this.getPropText(prop);
 	    }
 	}
+
+	elt.classList.toggle('smooth-colors');
     },
     updateSlider: function(i) {
 	if(i === null) {
