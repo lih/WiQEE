0cc5349786eaa1445d0b1578752bf238ea24bd97
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-11 19:05:21 +0000

    Deploy lih/WiQEE to github.com/lih/WiQEE.git:gh-pages

--- theme-tweak.js
+++ theme-tweak.js
@@ -68,8 +68,7 @@ var Theme = {
 	this.updateElement(rootI);
 
 	(function(amb,tw) {
-	    amb.value = this.ambiance;
-	    alert('Set ambiance value: '+amb.value);
+	    amb.value = tw.ambiance;
 	    amb.addEventListener('change', function() {
 		tw.setAmbiance(amb.value).updateAll();
 	    });
